072dae4db2f9ee2cd6bcab89fbe4d8a2fc7c84d3
Melissa Bryant <melissa.bryant.elt@gmail.com>
2023-07-05 09:18:39 +0200

    adding port

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfileGet(db));
 app.put('/image', image.handleImage(db));
 
-app.listen(3000, ()=> {
-    console.log('App is running on port 3000.');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`App is running on port ${process.env.PORT}.`);
 });
